d387ab4322a9be6a2cfafbc7275223fc34da2955
Ehud Gratia <148024632+ehudgratia@users.noreply.github.com>
2025-12-31 11:10:13 +0700

    poning

--- app/controllers/order.go
+++ app/controllers/order.go
@@ -19,28 +19,9 @@ func CreateOrder(ctx *fiber.Ctx) error {
 	userID := ctx.Locals("id").(int)
 	s := repository.GetService()
 
-	defer func() {
-		if r := recover(); r != nil {
-			err := s.Rollback(r)
-			ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"success": false,
-				"message": err.Error(),
-			})
-		}
-	}()
-
 	order, err := s.CreateOrder(ctx.Context(), userID, input)
 	if err != nil {
-		s.Rollback(err)
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success": false,
-			"message": err.Error(),
-		})
-	}
-
-	if err := s.Commit(); err != nil {
-		s.Rollback(err)
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"success": false,
 			"message": err.Error(),
 		})
